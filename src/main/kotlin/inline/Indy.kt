7ef95f3c69dbe521e82bf2aef56a7aebfb4e3064
Sergey Shkurov <seryoga6531@gmail.com>
2024-08-27 02:52:35 +0200

    Add indy

---
+++ src/main/kotlin/inline/Indy.kt
@@ -0,0 +1,9 @@
+package berlin.kotlin.inline
+
+fun main() {
+    val a = { println(123) }                        // since 2.0 or with -Xlambdas=indy
+    val b = @JvmSerializableLambda { println(456) } // before 2.0 or with -Xlambdas=class
+
+    println(a)
+    println(b)
+}
