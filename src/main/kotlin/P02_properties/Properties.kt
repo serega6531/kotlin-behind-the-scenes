2f8cda17d92c804527d6b2f6e350ec3f5eebe42a
Sergey Shkurov <seryoga6531@gmail.com>
2024-11-18 23:26:39 +0100

    Update Properties.kt

--- src/main/kotlin/P02_properties/Properties.kt
+++ src/main/kotlin/P02_properties/Properties.kt
@@ -3,10 +3,21 @@ package berlin.kotlin.properties
 val sameClassImmutable = 3
 var sameClassMutable = 5
 
+val withGetter: Int
+    get() = 8
+
 fun main() {
     println(sameClassImmutable)
 
     println(sameClassMutable)
     sameClassMutable = 10
     println(sameClassMutable)
+
+    println(withGetter)
+
+    println(AnotherClass().anotherClassImmutable)
+}
+
+private class AnotherClass {
+    val anotherClassImmutable = 12
 }
