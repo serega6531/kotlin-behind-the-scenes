79e7e30b5832b76db3d7a334f98e84a3ad31fe1f
Sergey Shkurov <seryoga6531@gmail.com>
2024-11-20 23:00:11 +0100

    Update Indy.kt

--- src/main/kotlin/P15_lambda/Indy.kt
+++ src/main/kotlin/P15_lambda/Indy.kt
@@ -2,11 +2,10 @@ package berlin.kotlin.lambda
 
 fun main() {
     val a = { println(123) }                        // since 2.0 or with -Xlambdas=indy
-    val b = @JvmSerializableLambda { println(456) } // before 2.0 or with -Xlambdas=class
-
     println(a)
-    println(b)
-
     a()
+
+    val b = @JvmSerializableLambda { println(456) } // before 2.0 or with -Xlambdas=class
+    println(b)
     b()
 }
