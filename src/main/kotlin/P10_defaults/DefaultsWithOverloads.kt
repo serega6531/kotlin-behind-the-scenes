3378048c1988f5bda594ae00261e851ac86fe53c
Sergey Shkurov <seryoga6531@gmail.com>
2024-11-20 02:33:39 +0100

    Split defaults module into two files

--- src/main/kotlin/P10_defaults/DefaultsWithOverloads.kt
+++ src/main/kotlin/P10_defaults/DefaultsWithOverloads.kt
@@ -1,5 +1,11 @@
 package berlin.kotlin.defaults
 
+fun main() {
+    doSomethingWithOverloads()
+    doSomethingWithOverloads(someBool = false)
+    doSomethingWithOverloads(someInt = 456)
+}
+
 @JvmOverloads
 fun doSomethingWithOverloads(someInt: Int = 123, someBool: Boolean = true, someString: String = "abc") {
     println("$someInt $someBool $someString")
